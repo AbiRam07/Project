fdede65407866278d2b9e12979cb5e01a11e3e37
ABINESH R <84151995+AbiRam07@users.noreply.github.com>
2025-08-04 16:47:46 +0530

    Update ping_all.sh

--- Ping/ping_all.sh
+++ Ping/ping_all.sh
@@ -1,5 +1,6 @@
 # Automation tool for Bulk_Ping
 
+```bash
 if [ -n "$1" ]; then 
 	target=$1
 		echo "\e[5m\e[41m\033[1;97m Make sure you have added new line in the ping file \033[0;37m"
@@ -11,9 +12,12 @@ if [ -n "$1" ]; then
 		if [ $? -eq 0 ]; then
 		echo "\e[42m\033[1;97m [+] Host [$domain] is up \033[0;37m"
 		output=$domain
-		echo "$output" >> Pingstatus.md
+		echo "$output" >> Pingstatus.txt
+		sleep 3
 		fi
 	done
 else
 	echo "\e[5m\e[41m\033[1;97m No target supplied \033[0;37m"
 fi
+```
+
